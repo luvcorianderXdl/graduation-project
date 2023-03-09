7ec4abd702df738a6ec49868ce4787ecdd8d1a12
Xdllll <xi_dao1ong@qq.com>
2023-03-09 09:50:38 +0800

    增加工具模块并修改依赖

---
+++ common-utils/src/test/java/com/qdu/commonutils/CommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qdu.commonutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
