bc7c75e347a7a954b6264f97649aad8173c8e94f
牛雍龙 <niuyonglong6@gmail.com>
2023-04-13 09:22:29 +0800

    后端获取session_key和openid

---
+++ wechat-applet/src/main/java/com/qdu/graduationProject/wechatApplet/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.qdu.graduationProject.wechatApplet.controller;
+
+import com.qdu.graduationProject.commonUtils.utils.JSONResult;
+import com.qdu.graduationProject.wechatApplet.vo.LoginRequestVo;
+import com.qdu.graduationProject.wechatApplet.voservice.LoginVoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/wxLogin")
+public class LoginController {
+    @Resource
+    private LoginVoService loginVoService;
+
+    @RequestMapping("/getSession")
+    @ResponseBody
+    public void getSession(HttpServletResponse resp,@RequestBody LoginRequestVo vo){
+        loginVoService.getSessionId(resp,vo);
+    }
+
+}
